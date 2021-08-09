325f463c5e6c3cbf75bfa7fda0220a6734ca746e
Majowicz24 <78666696+Majowicz24@users.noreply.github.com>
2021-08-09 12:56:13 -0400

    Create gameConfig.py

---
+++ code/gameConfig.py
@@ -0,0 +1,7 @@
+import discord
+from discord.ext import commands
+
+PREFIX = '-'
+bot = commands.Bot(command_prefix=PREFIX, description="Gambling Bot")
+client = commands.Bot(command_prefix="-")
+TOKEN = 'YOUR_TOKEN_HERE'
